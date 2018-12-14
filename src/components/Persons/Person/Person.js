9ba2baf954e86f93188d64978f9a8b032b835361
Casey Ross <caseyross003@gmail.com>
2018-12-13 19:01:57 -0600

    refactor ref to person input to use forwardRef

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -9,6 +9,7 @@ class Person extends Component {
   constructor(props) {
     super(props);
     console.log('[Person.js] Inside Constructor', props);
+    this.inputElement = React.createRef();
   }
 
   componentWillMount() {
@@ -17,9 +18,10 @@ class Person extends Component {
 
   componentDidMount() {
     console.log('[Person.js] Inside componentDidMount()');
-    if (this.props.position === 0) {
-      this.inputElement.focus();
-    }
+  }
+
+  focusInput() {
+    this.inputElement.current.focus();
   }
 
   render(){
@@ -30,7 +32,7 @@ class Person extends Component {
         <p onClick={this.props.click}>Hi, I'm {this.props.name} and I'm {this.props.age}!</p>
         <p>{this.props.children}</p>
         <input
-          ref={(input) => {this.inputElement = input}}
+          ref={this.inputElement}
           type="text"
           onChange={this.props.changed}
           value={this.props.name}/>
